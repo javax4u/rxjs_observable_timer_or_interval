b164c0619a955661557486cd67573b105a4b4d15
rupesh kumar <javax4u@gmail.com>
2020-08-26 15:51:32 +0530

    timer

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
+import { ObservableComponent } from './observable/observable.component';
+import { ListComponent } from './observable/list/list.component';
+import { FromEventComponent  } from './observable/from-event/from-event.component';
+import { IntervalComponent } from './observable/interval/interval.component';
+
+const routes: Routes = [
+  {path:'promise', component:PromiseComponent},
+  {path:'observable', component:ObservableComponent,children:[
+    {path:'',component:ListComponent},
+    {path:'',component:ListComponent},
+    {path:'from-event',component:FromEventComponent},
+    {path:'interval',component:IntervalComponent}
+  ]},
+  {path:'**',redirectTo:'promise'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
